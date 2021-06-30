86dad4fc2e0233ca50f2b8ac32e1726d53846701
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-30 22:25:28 +0200

    amenity added

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    Attributes:
+    name: Amenity's name
+    """
+    name = ""
