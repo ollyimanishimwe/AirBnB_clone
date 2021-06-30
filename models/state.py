21e97ca9ab61a9db042e910f403de3bf53a0d6a6
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-30 22:23:40 +0200

    state added

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class
+    Attributes:
+    name: State's name
+    """
+    name = ""
