6e84d290514bf78361252dc6a7dc6703f2f275e5
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-30 22:36:16 +0200

    city

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class
+    Attributes:
+    name: State's name
+    state_id: state id
+    """
+    name = ""
+    state_id = ""
